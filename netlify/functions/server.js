81b0db976676daa2ce566c2aad520f5b9b4a9037
Adarsh Mishra <adarshmishra01@Adarshs-MacBook-Air-2.local>
2026-05-05 22:19:31 +0530

    fix correct server export

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,30 +1,12 @@
 import server from "../../dist/server/index.js";
 
 export async function handler(event, context) {
-  try {
-    // If server itself is a function
-    if (typeof server === "function") {
-      return await server(event, context);
-    }
-
-    // If server has default handler
-    if (server && typeof server.default === "function") {
-      return await server.default(event, context);
-    }
-
-    // If server has fetch
-    if (server && typeof server.fetch === "function") {
-      return await server.fetch(event);
-    }
-
-    return {
-      statusCode: 500,
-      body: "No valid handler found in server",
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error.toString(),
-    };
+  if (server) {
+    return server(event);
   }
+
+  return {
+    statusCode: 500,
+    body: "Server function not working",
+  };
 }
